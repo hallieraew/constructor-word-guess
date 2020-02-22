caeb3dfacb94c105301b86c3b26629fa277e2391
hallieraew <hallieweintraub@gmail.com>
2020-02-22 17:30:39 -0600

    setting up initial files and setting up first constructor for letter.js

---
+++ letter.js
@@ -0,0 +1,30 @@
+var userInput = " ";
+
+function Letter (letterValue) {
+    this.letterValue = letterValue;
+    this.guessed = false;
+    this.letterCheck = function() {
+        userInput = process.argv[2];
+        if (letterValue === userInput) {
+            this.guessed = true;
+        } else {
+            this.guessed = false;
+    };
+}
+    this.letterDisplay = function() {
+        if (letterValue === userInput) {
+            return " ";
+        } else {
+            return "_";
+        }
+    }
+};
+
+var word = new Letter("k");
+
+word.letterCheck();
+console.log(word);
+console.log(userInput);
+
+
+// module.exports(Letter);
